0248a1212db7f7ad4288042abf0750ec6dabb595
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2026-04-01 12:22:41 +0200

    Uhr

--- 02_Canvas_Beispiele/js/grafics.mjs
+++ 02_Canvas_Beispiele/js/grafics.mjs
@@ -16,3 +16,20 @@ export function line(ctx, x1, y1, x2, y2, strokeStyle = "#fff", lineWidth = 1) {
     ctx.lineTo(x2, y2);
     ctx.stroke();
 }
+
+
+export function getTime() {
+    const now = new Date();
+    const sec = now.getSeconds();
+    const min = now.getMinutes();
+    let hrs = now.getHours();
+    if (hrs >= 12) hrs -= 12;
+    return {
+        sec,
+        min,
+        hrs,
+        pSec: sec.toString().padStart(2, "0"),
+        pMin: min.toString().padStart(2, "0"),
+        pHrs: hrs.toString().padStart(2, "0")
+    };
+}
--- 02_Canvas_Beispiele/main.mjs
+++ 02_Canvas_Beispiele/main.mjs
@@ -1,4 +1,4 @@
-import { circle, line } from "./js/grafics.mjs"
+import { circle, line, getTime } from "./js/grafics.mjs"
 
 window.onload = () => {
     const cnv = document.getElementById("cnv");
@@ -10,12 +10,46 @@ window.onload = () => {
     }
     window.addEventListener("resize", resize);
 
+    let alpha = 0;
+    const anglePerSecond = Math.PI / 30;
+    const anglePerHour = Math.PI / 6;
+
     function draw() {
+        const inner = cnv.height * 0.4;
+        const outer = cnv.height * 0.42;
         ctx.resetTransform();
         ctx.clearRect(0, 0, cnv.width, cnv.height);
 
-        ctx.fillStyle = "red";
-        ctx.fillRect(100, 100, 200, 20);
+        // Mitte des Fensters 
+        ctx.translate(cnv.width / 2, cnv.height / 2);
+
+        // 60 äußere Ticks
+        for (let i = 0; i < 60; i++) {
+            if (i % 5 === 0) {
+                line(ctx, inner, 0, outer + 15, 0, "#000", 2);
+            } else {
+                line(ctx, inner, 0, outer, 0, "#000", 1);
+            }
+            ctx.rotate(anglePerSecond);
+        }
+
+        const { pHrs, pMin, pSec, hrs, min, sec } = getTime();
+
+        // Zurücksetzen der Transformation in die Mitte
+        ctx.save();
+        // Rotation des Sekundenzeigers
+        ctx.rotate(sec * Math.PI / 30);
+        line(ctx, 0, 0, 0, -outer, "#f00", 1);
+        ctx.restore();
+
+        ctx.save();
+        ctx.rotate(min * Math.PI / 30);
+        line(ctx, 0, 0, 0, -outer, "#000", 3);
+        ctx.restore();
+
+        ctx.save();
+        ctx.rotate(hrs * Math.PI / 6);
+        line(ctx, 0, 0, 0, -outer / 2, "#000", 6);
 
         window.requestAnimationFrame(draw);
     }
