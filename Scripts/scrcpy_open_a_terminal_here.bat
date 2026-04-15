c9ddf5e615a35145517e05906b018b0a1d67b85a
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2026-04-15 12:41:34 +0200

    added batch script for scrcpy

---
+++ Scripts/scrcpy_open_a_terminal_here.bat
@@ -0,0 +1,32 @@
+@echo off
+
+echo Script Version 15.04.2026
+
+echo dev=adb devices
+echo rev=adb reverse tcp:3000 tcp:3000
+echo rmv=adb reverse --remove-all
+echo lsv=adb shell ls /sdcard/Oculus/VideoShots
+echo lss=adb shell ls /sdcard/Oculus/ScreenShots
+echo scs=scrcpy -m1024 
+
+doskey dev=adb devices
+
+:: adb reverse tcp:{{remote_port}} tcp:{{local_port}}
+doskey rev=adb reverse tcp:3000 tcp:3000
+doskey rmv=adb reverse --remove-all
+
+:: adb reverse --list
+
+doskey lsv=adb shell ls /sdcard/Oculus/VideoShots
+doskey lss=adb shell ls /sdcard/Oculus/ScreenShots
+doskey kll=adb kill-server
+
+
+set HERE=%~dp0
+cd /D %HERE%
+
+echo We are in %HERE%
+
+adb devices
+
+@cmd
