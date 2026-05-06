53b520a89701cf1dbdfc82c0fbfe84fc4e00951d
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2026-05-06 13:00:28 +0200

    grid of objects

--- 20_ThreeJS_Intro/app_with_map.mjs
+++ 20_ThreeJS_Intro/app_with_map.mjs
@@ -1,35 +1,57 @@
 import * as THREE from 'three';
+import { add } from './js/geometry.mjs';
+
 console.log("ThreeJs " + THREE.REVISION);
 
 const width = window.innerWidth, height = window.innerHeight;
 
-const camera = new THREE.PerspectiveCamera(70, width / height, 0.1, 10);
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10);
 camera.position.z = 1;
 
 const scene = new THREE.Scene();
 
 // Lichter
-scene.add(new THREE.HemisphereLight(0x808080, 0x606060));
+scene.add(new THREE.HemisphereLight(0xffffff, 0xffffff));
 const light = new THREE.DirectionalLight(0xffffff);
 light.position.set(0, 2, 0);
 scene.add(light);
 
 
 // Geometry
-const geometry = new THREE.BoxGeometry(0.1, 0.2, 0.2);
+// 
+const geometry = new THREE.TorusKnotGeometry(.05, .01, 50, 16);
 const material = new THREE.MeshNormalMaterial();
-const defaultCube = new THREE.Mesh(geometry, material);
+const defaultMesh = new THREE.Mesh(geometry, material);
 
-scene.add(defaultCube);
+scene.add(defaultMesh);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(width, height);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
+const objects = [], start = -1.2, stop = start * -1, step = 0.3;
+for (let x = start; x < stop; x += step) {
+    for (let y = start; y < stop; y += step) {
+        const o = (objects.length % 2 == 0) ? add(4, scene) : add(6, scene);
+        o.position.x = x;
+        o.position.y = y;
+        o.position.z = -2;
+        objects.push(o);
+    }
+}
+
+
 function animate(time) {
 
-    defaultCube.rotation.x = time / 2000;
-    // defaultCube.rotation.y = time / 1000;
+    defaultMesh.rotation.x = time / 2000;
+    defaultMesh.rotation.y = time / 1000;
+
+    for (const o of objects) {
+        o.rotation.x = time / 2000;
+        o.rotation.y = time / 1000;
+
+    }
+
     renderer.render(scene, camera);
 }
